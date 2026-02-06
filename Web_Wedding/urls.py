3a67e82d8fbbd0fb0b430299624a18339e152360
RobySaidi <robysaidi@MacBook-Air-RobySaidi.local>
2026-02-06 14:18:09 +0700

    Add root URL to index page

--- Web_Wedding/urls.py
+++ Web_Wedding/urls.py
@@ -1,11 +1,10 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),   # 👈 ROOT URL
     path('index/', views.index),
     path('about/', include('app_wedding.urls')),
-    
 ]
